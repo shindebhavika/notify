307b1ddb1392b94e637613178bb17df2701a8346
shindebhavika <you@example.com>
2024-07-31 15:20:34 +0530

    Your commit mess

--- app/send-notification/route.ts
+++ app/send-notification/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 // Initialize Firebase Admin SDK
 if (!admin.apps.length) {
-  const serviceAccount = require("@/service_key.json");
+  const serviceAccount = require('../../service_key.json'); // Adjust path as needed
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
@@ -18,7 +18,7 @@ export async function POST(request: NextRequest) {
     notification: {
       title: title,
       body: message,
-      imageUrl:"https://cdn-icons-png.flaticon.com/512/1827/1827272.png"
+      imageUrl: "https://cdn-icons-png.flaticon.com/512/1827/1827272.png"
     },
     webpush: link && {
       fcmOptions: {
@@ -29,7 +29,6 @@ export async function POST(request: NextRequest) {
 
   try {
     await admin.messaging().send(payload);
-
     return NextResponse.json({ success: true, message: "Notification sent!" });
   } catch (error) {
     return NextResponse.json({ success: false, error });
